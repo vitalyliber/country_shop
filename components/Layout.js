9ca8ec08ee1268c797c545ab0aa45a18788edfc5
vitaly.liber <zenamax@gmail.com>
2021-08-25 15:08:24 +0300

    fixed

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Head from 'next/head'
 const Layout = (props) => (
   <div>
     <Head>
-      <title>Торговый дом "ИП Моргунова"</title>
+      <title>Торговый дом &ldquo;ИП Моргунова&ldquo;</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" key="viewport" />
       <meta name="title" content="Автозапчасти в поселке Туртас" />
       <meta name="description" content="Большой выбор автозапчастей для отечественных и зарубежных автомобилей в продаже и под заказ в поселке Туртас Уватского района" />
